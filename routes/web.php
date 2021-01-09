20f7a78a4366a4a56d52e63a732ab4bf3feeea60
Semailk <sem.alik@mail.ru>
2021-01-09 13:47:48 +0300

    route commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::prefix('forum/')->group(function (){
+    Route::get('',[\App\Http\Controllers\HomeController::class,'index'])->name('forum.index');
+    Route::post('store',[\App\Http\Controllers\HomeController::class,'store'])->name('forum.store');
+});
+
+Route::prefix('admin/forum/')->middleware('auth')->group(function (){
+    Route::get('notviewed',[\App\Http\Controllers\Admin\AdminForumController::class,'notViewed'])->name('admin.forum.notViewed');
+    Route::get('viewed',[\App\Http\Controllers\Admin\AdminForumController::class,'viewed'])->name('admin.forum.viewed');
+    Route::get('update/{id}',[\App\Http\Controllers\Admin\AdminForumController::class,'update'])->name('admin.forum.update');
+    Route::get('delete/{id}',[\App\Http\Controllers\Admin\AdminForumController::class,'delete'])->name('admin.forum.delete');
+
+});
